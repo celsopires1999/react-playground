99f12161dbebad30b4600f49a648310ff3229031
celsopires1999 <celso.pires1999@gmail.com>
2021-11-30 21:05:29 +0000

    iniciando com temas e make Styles

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,36 @@
+import { AppBar, Button, makeStyles, Theme, Toolbar, Typography } from '@material-ui/core';
+import * as React from 'react';
+import logo from '../../static/img/logo.png'
+
+const useStyles = makeStyles((theme: Theme) => ({
+    toolbar: {
+        backgroundColor: '#000000',
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center',
+    },
+    login: {
+        backgroundColor: "#ffffff",
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]: {
+            width: 170
+        }
+    }
+}));
+
+export const Navbar: React.FC = () => {
+    const classes = useStyles();
+    return (
+        <AppBar>
+            <Toolbar className={classes.toolbar}>
+                <Typography className={classes.title}> 
+                    <img src={ logo } alt="CodeFlix" className={classes.logo} />
+                </Typography>
+                <Button className={classes.login}>Login</Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
