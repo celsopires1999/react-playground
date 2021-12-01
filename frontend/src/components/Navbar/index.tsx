453e8d4f55b74c5c9160ea9e27f000046406dfb9
celsopires1999 <celso.pires1999@gmail.com>
2021-11-30 22:14:37 -0300

    add começando com react hook - useState

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
-import { AppBar, Button, makeStyles, Theme, Toolbar, Typography } from '@material-ui/core';
 import * as React from 'react';
+import { AppBar, Button, IconButton, makeStyles, Menu, MenuItem, Theme, Toolbar, Typography } from '@material-ui/core';
 import logo from '../../static/img/logo.png'
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
@@ -23,9 +24,42 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export const Navbar: React.FC = () => {
     const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event: any) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
+                <IconButton 
+                    edge="start"
+                    color="inherit" 
+                    aria-controls="menu-appbar" 
+                    onClick={handleClick}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+                    transformOrigin={{vertical:'top', horizontal: 'center'}}
+                    getContentAnchorEl={null}
+                    >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
                 <Typography className={classes.title}> 
                     <img src={ logo } alt="CodeFlix" className={classes.logo} />
                 </Typography>
